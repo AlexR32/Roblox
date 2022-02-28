f6bab606d8e5ff6c7be5c764cf222f3bcad2ce16
AlexR32 <58864658+AlexR32@users.noreply.github.com>
2022-02-28 22:48:02 +0300

    added comments and cleaned code a bit

--- brainfuck_to_roblox.lua
+++ brainfuck_to_roblox.lua
@@ -1,28 +1,31 @@
 -- original credits to https://github.com/thirteen-io/brainfuck.lua
+-- helpful github io to convert text to brainfuck https://kleshni.github.io/Brainfuck-converter/
+-- slow as fuck with long code
 return function(code)
-	local comp = ""
-	local env = "local comp = {} local cell = 0 local cells = {} local function g() return (cells[cell] or 0) end local function s(a) cells[cell] = a end local function n() cell = cell + 1 end local function p() cell = cell - 1 end "
-	
-	for i = 1, string.len(code) do
-		local c = string.sub(code, i, i)
-		if ( c == "+" ) then
-			comp = comp.."s(g()+1)"
-		elseif ( c == "-" ) then
-			comp = comp.."s(g()-1)"
-		elseif ( c == ">" ) then
-			comp = comp.."n()"
-		elseif ( c == "<" ) then
-			comp = comp.."p()"
-		elseif ( c == "." ) then
-			comp = comp.."table.insert(comp,string.char(g()))"
-		elseif ( c == "," ) then
-			comp = comp.."s(string.byte(io.read(1)))"
-		elseif ( c == "[" ) then
-			comp = comp.."while not(g()==0)do "
-		elseif ( c == "]" ) then
-			comp = comp.."end "
-		end
-	end
-	
-	return env .. comp .. " loadstring(table.concat(comp,''))()"
+    -- env string (variables and functions to make compiled code work)
+    local env = "local comp = {} local cell = 0 local cells = {} local function g() return (cells[cell] or 0) end local function s(a) cells[cell] = a end local function n() cell = cell + 1 end local function p() cell = cell - 1 end "
+    local compile = ""
+
+    -- main part (compiler)
+    for index = 1, string.len(code) do
+        local sub = string.sub(code, index, index)
+        if sub == "+" then
+            compile = compile .. "s(g()+1)"
+        elseif sub == "-" then
+            compile = compile .. "s(g()-1)"
+        elseif sub == ">" then
+            compile = compile .. "n()"
+        elseif sub == "<" then
+            compile = compile .. "p()"
+        elseif sub == "." then
+            compile = compile .. "table.insert(comp,string.char(g()))"
+        elseif sub == "[" then
+            compile = compile .. "while not(g()==0)do "
+        elseif sub == "]" then
+            compile = compile .. "end "
+        end
+    end
+
+    -- env, compiled code and loadstring to combine all things
+    return env .. compile .. " loadstring(table.concat(comp,\"\"))()" -- returns kinda obfuscated code which can be executed with loadstring
 end
