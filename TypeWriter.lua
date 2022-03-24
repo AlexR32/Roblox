341e960cef1893b65822ceb2a01948de5fbb0b7f
AlexR32 <58864658+AlexR32@users.noreply.github.com>
2022-03-24 17:44:46 +0300

    Update TypeWriter.lua

--- TypeWriter.lua
+++ TypeWriter.lua
@@ -1,67 +1,18 @@
-local Module = {}
-
+local InsertService = game:GetService("InsertService")
+local HttpService = game:GetService("HttpService")
 local CoreGui = game:GetService("CoreGui")
-local Screen = CoreGui:FindFirstChild("NotificationScreen")
-local Folder = game:GetObjects("rbxassetid://7231968281")[1]
-if not Screen then
-	Screen = Folder.NotificationScreen:Clone()
-	Screen.Parent = CoreGui
+
+if not TW then
+	getgenv().TW = {}
+	TW.MainModule = InsertService:LoadLocalAsset("rbxassetid://7231968281")
+	TW.Screen = TW.MainModule.Screen:Clone()
+	TW.Screen.Name = "TW " .. HttpService:GenerateGUID(false)
+	TW.Screen.Parent = CoreGui
 end
 
-local function GetType(Variant, Type)
-	if type(Type) == "table" then
-		for Index, Value in ipairs(Type) do
-			if typeof(Variant) == Value then
-				return Variant
-			end
-		end
-	else
-		if typeof(Variant) == Type then
-			return Variant
-		end
-	end
-end
-
-local function GetLongest(A,B)
-	return A > B and A or B
-end
-
-function Module:Notification(Title,Description,Duration,Callback)
-	Title = GetType(Title,{"string","number"}) or "Title"
-	Description = GetType(Description,{"string","number"}) or "Description"
-	Callback = GetType(Callback,"function")
-	
-	local Notification = Folder.Notification:Clone()
-	Notification.Parent = Screen.Container
-	Notification.Title.Text = Title
-	Notification.Description.Text = Description
-	Notification.Title.Size = UDim2.new(1,0,0,Notification.Title.TextBounds.Y)
-	Notification.Description.Size = UDim2.new(1,0,0,Notification.Description.TextBounds.Y)
-	Notification.Size = UDim2.new(0,GetLongest(Notification.Title.TextBounds.X,Notification.Description.TextBounds.X) + 24,0,Notification.ListLayout.AbsoluteContentSize.Y + 8)
-	
-	if Duration then
-		task.spawn(function()
-			for Time = Duration,1,-1 do
-				Notification.Title.Close.Text = Time
-				task.wait(1)
-			end
-			Notification.Title.Close.Text = 0
-			
-			if Callback then
-				Callback()
-			end
-			Notification:Destroy()
-		end)
-	else
-		Notification.Title.Close.MouseButton1Click:Connect(function()
-			Notification:Destroy()
-		end)
-	end
-end
-
-function Module:TypeWrite(Text,Duration,Duration2)
-	local Message = Folder.Message:Clone()
-	Message.Parent = Screen.TypeWriter
+return function(Text,Duration,Duration2)
+	local Message = TW.MainModule.Message:Clone()
+	Message.Parent = TW.Screen
 	Message.Text = Text
 	Message.Size = UDim2.new(1,0,0,Message.TextBounds.Y)
 
@@ -69,8 +20,8 @@ function Module:TypeWrite(Text,Duration,Duration2)
 	local DisplayText = Text
 	DisplayText = DisplayText:gsub("<br%s*/>", "\n")
 	DisplayText = DisplayText:gsub("<[^<>]->", "")
-	
-	task.spawn(function()
+
+	coroutine.wrap(function()
 		for _ in utf8.graphemes(DisplayText) do
 			Index = Index + 1
 			Message.MaxVisibleGraphemes = Index
@@ -85,5 +36,3 @@ function Module:TypeWrite(Text,Duration,Duration2)
 		Message:Destroy()
 	end)
 end
-
-return Module
