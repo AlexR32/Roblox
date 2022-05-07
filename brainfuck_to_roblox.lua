5d5bb825a1c99689ed47e321479438c573264c13
AlexR32 <58864658+AlexR32@users.noreply.github.com>
2022-05-07 21:57:18 +0300

    Update brainfuck_to_roblox.lua

--- brainfuck_to_roblox.lua
+++ brainfuck_to_roblox.lua
@@ -3,29 +3,30 @@
 -- slow as fuck with long code
 return function(code)
     -- env string (variables and functions to make compiled code work)
-    local env = "local comp = {} local cell = 0 local cells = {} local function g() return (cells[cell] or 0) end local function s(a) cells[cell] = a end local function n() cell = cell + 1 end local function p() cell = cell - 1 end "
+    local env = "local comp,cells,cell={},{},0 local function get()return cells[cell]or 0 end local function set(v)cells[cell]=v end local function add()cell=cell+1 end local function sub()cell=cell-1 end "
     local compile = ""
 
     -- main part (compiler)
     for index = 1, string.len(code) do
         local sub = string.sub(code, index, index)
         if sub == "+" then
-            compile = compile .. "s(g()+1)"
+            compile = compile .. "set(get()+1)"
         elseif sub == "-" then
-            compile = compile .. "s(g()-1)"
+            compile = compile .. "set(get()-1)"
         elseif sub == ">" then
-            compile = compile .. "n()"
+            compile = compile .. "add()"
         elseif sub == "<" then
-            compile = compile .. "p()"
+            compile = compile .. "sub()"
         elseif sub == "." then
-            compile = compile .. "table.insert(comp,string.char(g()))"
+            compile = compile .. "table.insert(comp,string.char(get()))"
         elseif sub == "[" then
-            compile = compile .. "while not(g()==0)do "
+            compile = compile .. "while not(get()==0)do "
         elseif sub == "]" then
             compile = compile .. "end "
         end
     end
 
-    -- env, compiled code and loadstring to combine all things
-    return env .. compile .. " loadstring(table.concat(comp,\"\"))()" -- returns kinda obfuscated code which can be executed with loadstring
+    -- combining all things together (loadstring is for executing compiled code)
+    -- returns kinda obfuscated code which can be executed with loadstring
+    return env .. compile .. "loadstring(table.concat(comp,\"\"))()"
 end
